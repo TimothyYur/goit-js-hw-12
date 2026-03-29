b2cf39dace021169d5683324856be3eb0a13e5ac
TimothyYur <106112315+TimothyYur@users.noreply.github.com>
2026-03-29 12:27:35 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,19 +3,18 @@ import axios from 'axios';
 const API_KEY = '55201834-b2019b61559684ce41de74b7e';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page = 1) {
   const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    per_page: 15, // Вимога: 15 об'єктів
+    page: page,
   };
 
-  return axios
-    .get(BASE_URL, { params })
-    .then(response => response.data.hits)
-    .catch(error => {
-      throw new Error('Failed to fetch images');
-    });
+  const response = await axios.get(BASE_URL, { params });
+  // Повертаємо весь об'єкт data, щоб у main.js мати доступ до totalHits
+  return response.data;
 }
